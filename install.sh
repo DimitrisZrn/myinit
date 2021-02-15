826b20c4a7f52574d32ed1f8619155c1647a0845
dimitris <dimitris@pop-os.localdomain>
2021-02-15 09:36:31 +0200

    remove npm

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 cd $HOME
 REPOS="ppa:neovim-ppa/unstable"
-INSTALL_PKGS="python python3 python3-pip snapd npm clang-tools clangd-$1 neovim git"
+INSTALL_PKGS="python3 python3-pip snapd clang-tools clangd-$1 neovim git"
 
 sudo add-apt-repository $REPOS
 sudo apt update
@@ -10,7 +10,6 @@ sudo apt -y install  $INSTALL_PKGS
 pip3 install pynvim
 sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-$1 100
 snap install pyls
-npm i -g bash_language_server #nowork
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 #git clone "https://github.com/dzourn/nvim" $HOME/.config/
